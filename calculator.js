50cbf585e1027cec5b069486ae18d7237b58cfcd
j-west <westja89@gmail.com>
2016-12-02 14:44:39 -0600

    Added functions to add, subtract, multiply, divide, and a final function the takes two numbers and another function as arguments

---
+++ calculator.js
@@ -0,0 +1,20 @@
+// Create a function that multiplies two numbers passed in as arguments. Return the product.
+function product(num1, num2) {
+  return num1 * num2;
+}
+// Create a function that adds two numbers passed in as arguments. Return the sum.
+function sum(num1, num2) {
+  return num1 + num2;
+}
+// Create a function that subtracts two numbers passed in as arguments. Return the difference.
+function difference(num1, num2) {
+  return num1 - num2;
+}
+// Create a function that divides two numbers passed in as arguments. Return the quotient.
+function quitient(num1, num2) {
+  return num1 / num2;
+}
+
+function doSomething(num1, num2, func) {
+  return func(num1, num2);
+}
