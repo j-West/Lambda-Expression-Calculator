9e9c6fe782c3fe3b64ea2f403892cfc7a57ed7be
j-west <westja89@gmail.com>
2016-12-03 08:48:36 -0600

    Assign the contents of btn.div to buttonDiv variable.  Add event listener on the div and use conditional statements to decide which button was clicked

--- calculator.js
+++ calculator.js
@@ -1,14 +1,11 @@
 let number1 = document.querySelector("input");
 let number2 = document.querySelectorAll("input")[1];
-let buttons = document.querySelectorAll("button");
+var buttonDiv = document.querySelector(".btn-div");
 
-console.log(number1);
-console.log(number2);
-console.log(buttons);
-// Create a function that multiplies two numbers passed in as arguments. Return the product.
-function product(num1, num2) {
-  return num1 * num2;
-}
+// console.log(number1);
+// console.log(number2);
+// console.log(buttonDiv);
+
 // Create a function that adds two numbers passed in as arguments. Return the sum.
 function sum(num1, num2) {
   return num1 + num2;
@@ -25,3 +22,15 @@ function quitient(num1, num2) {
 function doSomething(num1, num2, func) {
   return func(num1, num2);
 }
+
+buttonDiv.addEventListener("click", (e) => {
+  if (e.target.value === "add") {
+    console.log(operations(parseInt(number1.value, 10), parseInt(number2.value, 10), add));
+  } else if (e.target.value === "subtract") {
+      console.log(operations(number1.value, number2.value, subtract));
+    } else if (e.target.value === "multiply") {
+        console.log(operations(number1.value, number2.value, multiply));
+      } else if (e.target.value === "divide"){
+          console.log(operations(number1.value, number2.value, divide));
+        }
+})
